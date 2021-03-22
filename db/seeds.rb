cf3b2712851df22bd3a5f30501225c3ad1e3fb05
ravi4rails <ravionrails@gmail.com>
2021-03-22 12:31:55 +0530

    initial commit wit dev model

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Developer.create(name: 'Vivek Anand Sharma');
+Developer.create(name: 'Kancan Kotiya');
+Developer.create(name: 'Shreya Vishwakarma');
+Developer.create(name: 'Ravi Kashyap');
+Developer.create(name: 'Shashi Kashyap');
+Developer.create(name: 'Payal Shamra');
+Developer.create(name: 'Shifali Rajput');
+Developer.create(name: 'Lokesh Maratha');
+Developer.create(name: 'Kapil Patidar');
